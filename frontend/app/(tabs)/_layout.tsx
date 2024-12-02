c5afaea1705111a72d2435f366ade64e0b42d275
alexquar <alexanderquarrie@gmail.com>
2024-12-02 14:18:29 -0500

    wipe pages

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -34,9 +34,16 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="upload"
         options={{
-          title: 'Explore',
+          title: 'Upload',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+        <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ frontend/app/(tabs)/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const About = () => {
+    return (
+        <View>
+         <p>
+            About the app
+            </p>   
+        </View>
+    );
+}
+
+
+export default About;
